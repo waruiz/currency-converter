5274171b55fe8766d21e155d644bcf48e01e838e
Will Ruiz <thisis.willruiz@gmail.com>
2018-06-30 16:03:28 -0700

    Add input and reverse currency buttons

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StatusBar } from 'react-native';
 
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
+import { InputWithButton } from '../components/TextInput';
+import { ClearButton } from '../components/Buttons';
 
-export default () => (
-  <Container>
-    <StatusBar translucent={false} barStyle="light-content" />
-    <Logo />
-  </Container>
-);
+const TEMP_BASE_CURRENCY = 'USD';
+const TEMP_QUOTE_CURRENCY = 'GBP';
+const TEMP_BASE_PRICE = '100';
+const TEMP_QUOTE_PRICE = '79.74';
+
+class Home extends Component {
+  handlePressBaseCurrency = () => {
+    console.log('press base');
+  };
+  handlePressQuoteCurrency = () => {
+    console.log('press quote');
+  };
+  handleTextChange = (text) => {
+    console.log('change text ', text);
+  };
+  handleSwapCurrency = () => {
+    console.log('press swap currency');
+  };
+  render() {
+    return (
+      <Container>
+        <StatusBar translucent={false} barStyle="light-content" />
+        <Logo />
+        <InputWithButton
+          buttonText={TEMP_BASE_CURRENCY}
+          onPress={this.handlePressBaseCurrency}
+          defaultValue={TEMP_BASE_PRICE}
+          keyboardType="numeric"
+          onChangeText={this.handleTextChange}
+        />
+        <InputWithButton
+          buttonText={TEMP_QUOTE_CURRENCY}
+          onPress={this.handlePressQuoteCurrency}
+          editable={false}
+          value={TEMP_QUOTE_PRICE}
+        />
+        <ClearButton text="Reverse Currencies" onPress={this.handleSwapCurrency} />
+      </Container>
+    );
+  }
+}
+
+export default Home;
