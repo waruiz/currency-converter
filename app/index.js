ad6a4c8ab6067a30dc772d672cfb7e13562dde48
Will Ruiz <thisis.willruiz@gmail.com>
2018-07-02 16:04:16 -0700

    Add app's universal styling colors

--- app/index.js
+++ app/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import Home from './screens/Home';
+
+import CurrencyList from './screens/CurrencyList';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
+
   $white: '#fff',
+  $lightGray: '#F0F0F0',
   $border: '#E2E2E2',
   $inputText: '#797979',
-  $lightGray: '#F0F0F0',
+  $darkText: '#343434',
+
+  $outline: 1,
 });
 
-export default () => <Home />;
+export default () => <CurrencyList />;
