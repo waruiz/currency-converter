8fc6d2b3ec1235e752d28e1c023c5390635423ec
Will Ruiz <thisis.willruiz@gmail.com>
2018-07-06 17:27:31 -0700

    Ignore Redux logger for navigation changes

--- app/index.js
+++ app/index.js
@@ -24,7 +24,7 @@ EStyleSheet.build({
 export default () => (
   <Provider store={store}>
     <AlertProvider>
-      <Navigator />
+      <Navigator onNavigationStateChange={null} />
     </AlertProvider>
   </Provider>
 );
