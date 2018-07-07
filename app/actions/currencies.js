fe0b491a43fc302af70707a43444838e2d0c8612
Will Ruiz <thisis.willruiz@gmail.com>
2018-07-06 20:51:47 -0700

    Create change currency actions

--- app/actions/currencies.js
+++ app/actions/currencies.js
@@ -1,4 +1,4 @@
-import { SWAP_CURRENCY, CHANGE_CURRENCY_AMOUNT } from './types';
+import { SWAP_CURRENCY, CHANGE_CURRENCY_AMOUNT, CHANGE_BASE_CURRENCY, CHANGE_QUOTE_CURRENCY } from './types';
 
 export const swapCurrency = () => ({
   type: SWAP_CURRENCY,
@@ -7,4 +7,14 @@ export const swapCurrency = () => ({
 export const changeCurrencyAmount = amount => ({
   type: CHANGE_CURRENCY_AMOUNT,
   amount: parseFloat(amount),
+});
+
+export const changeBaseCurrency = currency => ({
+  type: CHANGE_BASE_CURRENCY,
+  currency,
+});
+
+export const changeQuoteCurrency = currency => ({
+  type: CHANGE_QUOTE_CURRENCY,
+  currency,
 });
