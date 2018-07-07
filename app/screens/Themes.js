48ce2a1e517e3881724b5cb955c8dd5c35506146
Will Ruiz <thisis.willruiz@gmail.com>
2018-07-06 20:50:43 -0700

    Connect Themes screen to Redux; dispatch action on theme selection

--- app/screens/Themes.js
+++ app/screens/Themes.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { ScrollView, StatusBar } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import { connect } from 'react-redux';
 
 import { ListItem, Separator } from '../components/List';
+import { changePrimaryColor } from '../actions/theme';
 
 const styles = EStyleSheet.create({
   $blue: '$primaryBlue',
@@ -15,10 +17,11 @@ const styles = EStyleSheet.create({
 class Themes extends Component {
   static propTypes = {
     navigation: PropTypes.object,
+    dispatch: PropTypes.func,
   };
 
   handleThemePress = (color) => {
-    console.log('press theme ', color);
+    this.props.dispatch(changePrimaryColor(color));
     this.props.navigation.goBack(null);
   };
 
@@ -67,4 +70,4 @@ class Themes extends Component {
   }
 }
 
-export default Themes;
+export default connect()(Themes);
